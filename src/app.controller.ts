8c76920284323e4d33dd0ec90c00beb82839f487
Mai El-Kholy <mai.elkholy1318@gmail.com>
2024-12-10 19:57:01 +0200

    adding swagger to project + fixing prettier format errors

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Setup')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @ApiOperation({ summary: 'Get example data' }) // Add a summary to this endpoint
+  @ApiResponse({ status: 200, description: 'Successfully retrieved data.' })
   getHello(): string {
     return this.appService.getHello();
   }
